ffa761f2c53bbe6033d85ea162701686e64ac208
Scott <scotthenry1@gmail.com>
2023-01-18 16:15:28 -0500

    Initial lab submission

---
+++ index.js
@@ -0,0 +1,17 @@
+function introduction(name) {
+    return (`Hi, my name is ${name}.`);
+  }
+
+  function introductionWithLanguageOptional(name, language = "JavaScript") {
+    return (`Hi, my name is ${name} and I am learning to program in ${language}.`);
+  }
+
+  function introductionWithLanguage(name, language) {
+    return (`Hi, my name is ${name} and I am learning to program in ${language}.`);
+  }
+
+  function sayHelloTo(firstName = "User") {
+    console.log(`Hello, ${firstName}!`);
+  }
+
+  
